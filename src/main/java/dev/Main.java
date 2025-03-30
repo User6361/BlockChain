575b2511f35817e7cf10cba582288c5833148149
User6361 <tomasshelbyy098@gmail.com>
2025-03-30 15:23:53 +0300

    Test commit 2

--- src/main/java/dev/Main.java
+++ src/main/java/dev/Main.java
@@ -4,9 +4,15 @@ package dev;
 
 //Few changes for commit
 public class Main {
+
+
+    public static final int X = 1;
+
     public static void main(String[] args) {
         printT("Hello");
     }
+
+
 
     public static void printT(String str){
         System.out.println(str);
